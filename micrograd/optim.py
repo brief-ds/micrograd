1c538c8ea68a39a2016aadaab8b83e352d658d11
Jencir Lee <jl@brief-ds.com>
2026-05-07 11:31:06 +0100

    add ADAM optimizer

--- micrograd/optim.py
+++ micrograd/optim.py
@@ -23,19 +23,16 @@ class SGD:
         learning_rate: a non-negative number or a generator of them.
         momentum: None or a non-negative number.
         '''
-        assert isinstance(wrt, (list, tuple))
         assert all([isinstance(_, Value) for _ in wrt])
         if isinstance(learning_rate, Number):
             assert learning_rate >= 0
-        else:
-            assert hasattr(learning_rate, '__next__')
         assert momentum is None or momentum >= 0
 
         self.wrt = wrt
         self.learning_rate = learning_rate
         self.learning_rate_is_number = isinstance(learning_rate, Number)
         self.momentum = momentum
-        if momentum is not None:
+        if momentum:
             self.velocity = [zeros(_.shape, dtype=DTYPE)
                              for _ in wrt]
 
@@ -55,3 +52,42 @@ class SGD:
                 self.velocity[j] *= self.momentum
                 self.velocity[j] += v.grad
                 v.data -= lr * self.velocity[j]
+
+
+class ADAM:
+    def __init__(self, wrt=[], learning_rate=None,
+                 beta1=None, beta2=None, eps_adam=None):
+        ''' eg learning_rate = .001, beta1 = .9,
+                beta2 = .999, eps_adam = 1e-8,
+
+            or learning_rate = .01, beta1 = .85,
+                beta2 = .99, eps_adam = 1e-8
+        '''
+        assert learning_rate >= 0
+        assert beta1 >= 0
+        assert beta2 >= 0
+        assert eps_adam >= 0
+
+        self.wrt = wrt
+        self.learning_rate = learning_rate
+        self.beta1 = beta1
+        self.beta2 = beta2
+        self.eps_adam = eps_adam
+        # first moments
+        self.m = [zeros(_.shape, dtype=DTYPE) for _ in wrt]
+        # second moments
+        self.v = [zeros(_.shape, dtype=DTYPE) for _ in wrt]
+
+    def step(self, step, num_steps):
+        assert 0 <= step < num_steps
+
+        lr = self.learning_rate * (1 - step / num_steps)
+
+        for j, v in enumerate(self.wrt):
+            self.m[j] *= self.beta1
+            self.m[j] += (1 - self.beta1) * v.grad
+            self.v[j] *= self.beta2
+            self.v[j] += (1 - self.beta2) * v.grad ** 2
+            m_hat = self.m[j] / (1 - self.beta1 ** (step + 1))
+            v_hat = self.v[j] / (1 - self.beta2 ** (step + 1))
+            v.data -= lr * m_hat / (v_hat ** .5 + self.eps_adam)
